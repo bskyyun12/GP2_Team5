df1673cdae933454c2f442246f88dfe85ff83b99
David Liljefors <david.liljefors@futuregames.nu>
2020-08-28 00:31:29 +0200

    more laser stuff

--- Source/GP2_Team5/LightEmitter.cpp
+++ Source/GP2_Team5/LightEmitter.cpp
@@ -24,7 +24,20 @@ void ALightEmitter::BeginPlay()
 void ALightEmitter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	FVector Start = GetActorLocation();
+	if (bIsCCW)
+	{
+		SendLaserCCW(GetActorLocation(), 0);
+	}
+	else
+	{
+		SendLaserCW(GetActorLocation(), 0);
+	}
+}
+
+
+bool ALightEmitter::SendLaserCCW(FVector Start, int Bounces)
+{
+	if (Bounces > MaxBounces) return false;
 
 	float DistanceFromCenter = Start.Size();
 	Start.Normalize();
@@ -33,23 +46,137 @@ void ALightEmitter::Tick(float DeltaTime)
 
 	QuantizationLevel = FMath::Max(QuantizationLevel, 0.05f);
 
-	for (float i = angle; i < PI + angle; i+= QuantizationLevel)
+	for (float i = angle; i < 2.F * PI + angle; i += QuantizationLevel)
 	{
-		const float startY = FMath::Sin(i) * DistanceFromCenter;
-		const float startZ = FMath::Cos(i) * DistanceFromCenter;
-		const float endY = FMath::Sin(i+ QuantizationLevel) * DistanceFromCenter;
-		const float endZ = FMath::Cos(i+ QuantizationLevel) * DistanceFromCenter;
+		const float StartY = FMath::Sin(i) * DistanceFromCenter;
+		const float StartZ = FMath::Cos(i) * DistanceFromCenter;
+		const float EndY = FMath::Sin(i + QuantizationLevel) * DistanceFromCenter;
+		const float EndZ = FMath::Cos(i + QuantizationLevel) * DistanceFromCenter;
 
-		const FVector start = { 0.0F, startY, startZ };
-		const FVector end = { 0.0F, endY, endZ };
+		const FVector StartPoint = { 0.0F, StartY, StartZ };
+		const FVector EndPoint = { 0.0F, EndY, EndZ };
 
-		DrawDebugLine(GetWorld(), start, end, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
+		DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
 		FHitResult Hit;
-		auto result = GetWorld()->LineTraceSingleByChannel(Hit, start, end, ECollisionChannel::ECC_Visibility);
-		if (result)
+		bool bHitSomething = GetWorld()->LineTraceSingleByChannel(Hit, StartPoint, EndPoint, ECollisionChannel::ECC_Visibility);
+		if (bHitSomething)
 		{
-			UE_LOG(LogTemp, Warning, TEXT("we hit stuff"));
-			break;
+			UE_LOG(LogTemp, Warning, TEXT("Send CW hit something"));
+
+			float HitPlaneAngle = FVector::DotProduct(Hit.ImpactPoint.GetSafeNormal(), Hit.Normal);
+
+			if (HitPlaneAngle > -0.707)
+			{
+				return SendLaserDown(Hit.ImpactPoint, Bounces + 1);
+			}
+			else
+			{
+				return SendLaserUp(Hit.ImpactPoint, Bounces + 1);
+			}
+
 		}
 	}
+	return false;
+}
+
+bool ALightEmitter::SendLaserCW(FVector Start, int Bounces)
+{
+	if (Bounces > MaxBounces) return false;
+
+	float DistanceFromCenter = Start.Size();
+	Start.Normalize();
+
+	float Angle = FMath::Atan2(Start.Y, Start.Z);
+
+	QuantizationLevel = FMath::Max(QuantizationLevel, 0.05f);
+
+	for (float i = Angle; i > Angle - 2.F*PI; i -= QuantizationLevel)
+	{
+		const float StartY = FMath::Sin(i) * DistanceFromCenter;
+		const float StartZ = FMath::Cos(i) * DistanceFromCenter;
+		const float EndY = FMath::Sin(i - QuantizationLevel) * DistanceFromCenter;
+		const float EndZ = FMath::Cos(i - QuantizationLevel) * DistanceFromCenter;
+
+		const FVector StartPoint = { 0.0F, StartY, StartZ };
+		const FVector EndPoint = { 0.0F, EndY, EndZ };
+
+		DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
+		FHitResult Hit;
+		bool bHitSomething = GetWorld()->LineTraceSingleByChannel(Hit, StartPoint, EndPoint, ECollisionChannel::ECC_Visibility);
+		if (bHitSomething)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Send CW hit something"));
+
+			float HitPlaneAngle = FVector::DotProduct(Hit.ImpactPoint.GetSafeNormal(), Hit.Normal);
+
+			if (HitPlaneAngle > -0.707)
+			{
+				return SendLaserDown(Hit.ImpactPoint, Bounces + 1);
+			}
+			else
+			{
+				return SendLaserUp(Hit.ImpactPoint, Bounces + 1);
+			}
+			
+		}
+	}
+	return false;
+}
+
+bool ALightEmitter::SendLaserUp(FVector Start, int Bounces)
+{
+	if (Bounces > MaxBounces) return false;
+	FVector UpVector = Start.GetSafeNormal();
+
+	FVector End = Start + 1000.F * UpVector;
+
+	
+	FHitResult Hit;
+	bool bHitSomething = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_Visibility);
+	if (bHitSomething)
+	{
+		DrawDebugLine(GetWorld(), Start, Hit.ImpactPoint, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
+		float HitPlaneAngle = FVector::DotProduct(Hit.ImpactPoint.GetSafeNormal(), Hit.Normal);
+
+		UE_LOG(LogTemp, Warning, TEXT("Send up hit something angle : %f"), HitPlaneAngle);
+		if (HitPlaneAngle >	-0.707F)
+		{
+			return SendLaserCCW(Hit.ImpactPoint, Bounces + 1);
+		}
+		else
+		{
+			return SendLaserCW(Hit.ImpactPoint, Bounces + 1);
+		}
+	}
+	DrawDebugLine(GetWorld(), Start, End, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
+	return false;
+}
+
+bool ALightEmitter::SendLaserDown(FVector Start, int Bounces)
+{
+	if (Bounces > MaxBounces) return false;
+
+	FVector UpVector = Start.GetSafeNormal();
+	FVector End = Start - 1000.F * UpVector;
+
+	FHitResult Hit;
+	bool bHitSomething = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_Visibility);
+	if (bHitSomething)
+	{
+		DrawDebugLine(GetWorld(), Start, Hit.ImpactPoint, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
+		float HitPlaneAngle = FVector::DotProduct(Hit.ImpactPoint.GetSafeNormal(), Hit.Normal);
+
+		UE_LOG(LogTemp, Warning, TEXT("Send Down hit something angle : %f"), HitPlaneAngle);
+		if (HitPlaneAngle > -0.707F)
+		{
+			return SendLaserCCW(Hit.ImpactPoint, Bounces + 1);
+		}
+		else
+		{
+			return SendLaserCW(Hit.ImpactPoint, Bounces + 1);
+		}
+	}
+
+	DrawDebugLine(GetWorld(), Start, End, FColor(255, 0, 0), false, 0.1f, 0, 3.f);
+	return false;
 }
--- Source/GP2_Team5/LightEmitter.h
+++ Source/GP2_Team5/LightEmitter.h
@@ -12,18 +12,23 @@ class GP2_TEAM5_API ALightEmitter : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	ALightEmitter();
 
-protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	bool SendLaserCCW(FVector Start, int Bounces);
+	bool SendLaserCW(FVector Start, int Bounces);
+
+	bool SendLaserUp(FVector Start, int Bounces);
+	bool SendLaserDown(FVector Start, int Bounces);
+
+protected:
 	UPROPERTY(Editanywhere, BlueprintReadWrite)
 	float QuantizationLevel = 0.1F;
 
+	UPROPERTY(Editanywhere)
+	bool bIsCCW = false;
+
+	int32 MaxBounces = 12;
 };
