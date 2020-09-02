34dabecd3b4372005db426e0f576f08b7782a1f6
DESKTOP-VRBG78Q\jokke <jockes.ljud@gmail.com>
2020-09-02 14:16:39 +0200

    stuff

---
+++ Source/GP2_Team5/AiInterfaceMechnics.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AiInterfaceMechnics.h"
+
+// Add default functionality here for any IAiInterfaceMechnics functions that are not pure virtual.
--- Source/GP2_Team5/GP2_Team5.Build.cs
+++ Source/GP2_Team5/GP2_Team5.Build.cs
@@ -8,15 +8,6 @@ public class GP2_Team5 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { 
-			"Core", 
-			"CoreUObject", 
-			"Engine", 
-			"InputCore", 
-			"HeadMountedDisplay", 
-			"UMG", 
-			"AIModule",
-			"ApexDestruction",
-		});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "AIModule" });
 	}
 }
