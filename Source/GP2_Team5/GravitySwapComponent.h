185d355d4757ff03e1407ce698efaf73786a55d7
David Liljefors <david.liljefors@futuregames.nu>
2020-09-03 17:57:54 +0200

    fix so this builds haha yes :100: :+1:

--- Source/GP2_Team5/GravitySwapComponent.h
+++ Source/GP2_Team5/GravitySwapComponent.h
@@ -20,7 +20,6 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
 public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
